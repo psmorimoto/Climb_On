b32d6aa23bcebadd8da8d04e3af86e2473aff547
Paul Morimoto <psmorimoto@gmail.com>
2019-03-27 12:27:30 -0700

    Display AddClimb form as a pop up Modal

--- client/components/AddClimb.jsx
+++ client/components/AddClimb.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ClimbGrades from './ClimbGrades.jsx';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 export default class AddClimb extends React.Component {
   constructor(props) {
@@ -17,7 +17,7 @@ export default class AddClimb extends React.Component {
   }
 
   handleModal() {
-    this.setState({showModal: true});
+    this.setState(prevState => ({showModal: !prevState.showModal}));
   }
 
   handleClimbType(e) {
@@ -34,25 +34,34 @@ export default class AddClimb extends React.Component {
     } else {
       return (
         <div>
-          <Form>
-            <FormGroup>
-              <Label for="exampleClimbType">Climbing Type</Label>
-              <Input type="select" name="climbType" id="SelectClimbType" onChange={this.handleClimbType}>
-                <option>Bouldering</option>
-                <option>Top Rope</option>
-                <option>Lead</option>
-                </Input>
-            </FormGroup>
-            <ClimbGrades climbType={this.state.climbType} handleClimbType={this.handleClimbType} />
-            <FormGroup>
-              <Label for="ClimbLocation">Location</Label>
-              <Input type="text" name="climbLocation" onChange={this.handleClimbType}></Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="ClimbDescription">Description/Notes</Label>
-              <Input type="textarea" name="climbDescription" onChange={this.handleClimbType}></Input>
-            </FormGroup>
-          </Form>
+          <Modal isOpen={this.state.showModal} toggle={this.handleModal}>
+            <ModalHeader>Add Your Climb!</ModalHeader>
+            <ModalBody>
+              <Form>
+                <FormGroup>
+                  <Label for="exampleClimbType">Climbing Type</Label>
+                  <Input type="select" name="climbType" id="SelectClimbType" onChange={this.handleClimbType}>
+                    <option>Bouldering</option>
+                    <option>Top Rope</option>
+                    <option>Lead</option>
+                    </Input>
+                </FormGroup>
+                <ClimbGrades climbType={this.state.climbType} handleClimbType={this.handleClimbType} />
+                <FormGroup>
+                  <Label for="ClimbLocation">Location</Label>
+                  <Input type="text" name="climbLocation" onChange={this.handleClimbType}></Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="ClimbDescription">Description/Notes</Label>
+                  <Input type="textarea" name="climbDescription" onChange={this.handleClimbType}></Input>
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.handleModal}>Submit Climb</Button>
+              <Button color="secondary" onClick={this.handleModal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
 
           <div>
             {this.state.climbType} --
